6cf00acf58b7558fc178a1f9a22d96e3abda10c3
Pavel Hübner <hubnerp@btlnet.com>
2016-11-05 23:39:58 +0100

    SRS update

--- App/main.cpp
+++ App/main.cpp
@@ -20,31 +20,29 @@ int main(int argc, char *argv[])
   a) ok - volitelný čas práce 0 - xx minut
   b) ok - volitelný čas přestávky
   c) ok - schovat do tray baru, obnovit z traybaru
-  d)      neprodávat to za $25
-  e) in - kontext menu na pravé kliknutí (možnost Really Exit, add 5 minutes, open)
-  f)      nezaměřovat to přímo na RSI, ale jsou i jiné problémy (např. protáhnout si nohy, zakroutit hlavou => vybízí to k sepsání několika tipů (nechtěl jsem napsat něco jiného?))
-  g)      grafy - grafy čeho? Využití myši, pravé nebo levé klávesnice? Není to zbytečně složité? Nebude to děsit uživatele?
+  e) ok - kontext menu na pravé kliknutí (možnost Really Exit, add 5 minutes, open)
+  w) ok - saving data in QSettings
   h)      update over internet
   i) ok - tolerance - např. stisknutí klávesy nezastaví cooldown (zastaví, pokud není nucená pauza)
-          -pokud je idle pod toleranci, idle se neustále resetuje. Je to dobře? Definovat?
   j)      okénko pro test reporty a feedback over internet a donate (paypal, bitcoin, ...)
   k)      hezčí a nativní grafika
-  l)      měření celkového času u počítače? Měřit počet period? Do kdy? Resetovat na požádání? Na úspání počítače?
   m)      run at startup checkbox - http://stackoverflow.com/questions/9534415/run-application-on-startup (C:\Users\hubnerp\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup)
-  n)      translations
   o)      event driven
-  p)      sound signalisation
+  p)      sound signalisation (on rest time, on the start of work period)
   q)      united graphics
   r)      little numbers of times?
   q)      menu
   u)      better name
   v)      always on top
-  w) ok - saving data in QSettings
-  x)      bug in tolerance setting (it counts plus one)
+  x)      bug in tolerance setting (it counts plus one, does it still?)
   y)      info about running in tray system
-  w)      resetuje se bez pipani pri prekroceni worktime
   z)      write unit tests
   aa)     versioning - http://www.openguru.com/2009/11/qt-best-way-to-set-application-version.html
+  ab)     neprodávat to za $25
+  ac)     nezaměřovat to přímo na RSI, ale jsou i jiné problémy (např. protáhnout si nohy, zakroutit hlavou => vybízí to k sepsání několika tipů (nechtěl jsem napsat něco jiného?))
+  ad)     grafy - grafy čeho? Využití myši, pravé nebo levé klávesnice? Není to zbytečně složité? Nebude to děsit uživatele? Statistika (pocet period)
+  ae)     translations
+  af)     měření celkového času u počítače? Měřit počet period? Do kdy? Resetovat na požádání? Na úspání počítače?
   */
 
 // http://doc.qt.io/qt-5/qtwidgets-desktop-systray-example.html
